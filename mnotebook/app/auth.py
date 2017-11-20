f4b90450d8b1d2deec0ed3e3643dc9b07aefe1e3
Tong Zhang <zhangt@frib.msu.edu>
2017-11-20 09:50:55 -0500

    Change authentication policy, support session.

--- mnotebook/app/auth.py
+++ mnotebook/app/auth.py
@@ -1,6 +1,7 @@
 from flask_httpauth import HTTPBasicAuth
 from flask import make_response
 from flask import jsonify
+from flask import session
 
 from passlib.apps import custom_app_context
 
@@ -9,18 +10,34 @@ from .models import Admin
 
 auth = HTTPBasicAuth()
 
-users = {
-    'compdev': '$6$rounds=656000$PGX0OHSnmJYvJgUQ$riSaArDbDYQCKcsyy03./jt6seAlSh7jmlF3JhXSITXI8m8gRXqx4yT.M9Poao6CL8vRq/ECnehLbTsfhtbcf0',
-}
 
 @auth.verify_password
 def verify_password(username, password):
+    # if not found logged in admins, pop up authentication dialog
+    if 'logged_in_admin' in session and session['logged_in_admin']:
+        return True
+    else:
+        u = Admin.query.filter(Admin.nickname==username).first()
+        if not u:
+            return False
+        if u.verify_password(password):
+            session['logged_in_admin'] = u.nickname
+            return True
+        return False
+    """
     # user or admin
     u = Admin.query.filter(Admin.nickname==username).first()
     if not u:
         return False
-    return u.verify_passwd(password)
+    if u.verify_password(password):
+        session['logged_in_admin'] = True
+        return True
+    else:
+        session['logged_in_admin'] = False
+        return False
+    #return u.verify_password(password)
     #return custom_app_context.verify(password, users.get(username))
+    """
 
 @auth.error_handler
 def unauthorized():
