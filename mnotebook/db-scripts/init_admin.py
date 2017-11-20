f7395dc3036faf2b1531c69b2e4d2c389cc9e66c
Tong Zhang <zhangt@frib.msu.edu>
2017-11-20 10:27:55 -0500

    Update script for admin initialization.

--- mnotebook/db-scripts/init_admin.py
+++ mnotebook/db-scripts/init_admin.py
@@ -7,12 +7,14 @@
 import sys
 sys.path.insert(0, '../')
 
-from app.models import User
+from app.models import Admin
 from app.models import db
 
+_admin0 = {'name': 'compadmin', 'hash': '$6$rounds=656000$3bP2.doJoQbi4JBi$SftaJWRdM8crEFjVcZ2Oa7GmpoHOQsRQuWLPO2JMyaUti8UhuYM9LOO2GSWoLUVpy9prcoleqVY.K.rC8okVG0'}
 
-u1 = User(nickname='dev1', email='dev1@localhost', id=1)
+admin0 = Admin(nickname=_admin0['name'], password_hash=_admin0['hash'], 
+               email='admin@localhost', id=1)
 
-# users:
-db.session.add(u1)
+# default admin account
+db.session.add(admin0)
 db.session.commit()
