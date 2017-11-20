fa74987610ced238d5c9c1b0ba261d430daac3e2
Tong Zhang <zhangt@frib.msu.edu>
2017-11-20 09:51:45 -0500

    Add functions to support container model.
    - map dict to manage input parameters of 'phyapps' container.

--- mnotebook/app/utils.py
+++ mnotebook/app/utils.py
@@ -10,6 +10,23 @@ import docker
 client = docker.from_env()
 container_fields_of_interest = ('name', 'id', 'status', 'image')
 
+# container image names mapping:
+cname_map = {
+        'phyapps:1.5-ss': 'tonyzhang/phyapps:release-1.5-ss',
+        'phyapps:1.5': 'tonyzhang/phyapps:release-1.5',
+}
+
+# accelerator section names mapping:
+mach_map = {
+        'LEBT': 'FRIB_LEBT',
+        'MEBT': 'FRIB_MEBT',
+        'LS1': 'FRIB_LINAC',
+}
+
+# initla port number:
+NB_PORT, SS_PORT = 31000, 32000
+
+
 def utc2local(u_time):
     l_tz = tzlocal.get_localzone()
     l_time = u_time.replace(tzinfo=pytz.utc).astimezone(l_tz)
@@ -40,18 +57,80 @@ def get_container_name(cid):
         return "Unknown"
 
 
+def get_container_image(cid):
+    try:
+        return client.containers.get(cid).image
+    except:
+        return "Unknown"
+
+
+def get_container_ports(cid):
+    try:
+        c = client.containers.get(cid)
+        ps = c.attrs['NetworkSettings']['Ports']
+        return [v[0]['HostPort'] for k,v in ps.items()]
+    except:
+        return None
+
+
+def get_container_ctime(cid):
+    try:
+        c = client.containers.get(cid)
+        t0 = c.attrs['Created']
+        t0_obj = datetime.strptime(t0[0:-4], '%Y-%m-%dT%H:%M:%S.%f')
+        return utc2local(t0_obj)
+    except:
+        return 'Unknown'
+
+
+def get_container_uptime(cid):
+    try:
+        c = client.containers.get(cid)
+        time0 = c.attrs['Created']
+        t0 = datetime.strptime(time0[0:-4], '%Y-%m-%dT%H:%M:%S.%f')
+        t1 = datetime.utcnow()
+        return str(t1-t0)
+    except:
+        return 'Unknown'
+
+
+def get_container_shortid(cid):
+    try:
+        return client.containers.get(cid).short_id
+    except:
+        return  'Unknown'
+
+
+def get_container_url(cid, port=8888):
+    """ Return server urls.
+    """
+    try:
+        c = client.containers.get(cid)
+        ps = c.attrs['NetworkSettings']['Ports']
+        all_urls = {
+                k: 'http://{0}:{1}'.format(
+                    v[0]['HostIp'], v[0]['HostPort'])
+                for k,v in ps.items()
+        }
+        return all_urls.get('{}/tcp'.format(port))
+    except:
+        return 'Unknown'
+        
+
 def request_json():
     best = request.accept_mimetypes \
             .best_match(['application/json', 'text/html'])
     return best == 'application/json'
 
 
-def validate_container(cid):
+def validate_container(name):
+    """Validate container with input name, return id and container itself.
+    """
     try:
-        container = client.containers.get(cid)
-        return container, {k:getattr(container, k) for k in container_fields_of_interest}
+        c = client.containers.get(name)
+        return c.id, c
     except:
-        return None, False
+        return None, None
 
 
 def get_container(cid):
@@ -59,3 +138,56 @@ def get_container(cid):
         return client.containers.get(cid)
     except:
         return None
+
+
+def create_new_container(data):
+    """Create new container.
+    
+    Parameters
+    ----------
+    data : dict
+        'image': image name, other keys: parameters.
+
+    Returns
+    -------
+    (id, name, nb_url, ss_url): tuple
+        id: container id.
+        name: container name.
+        nb_url: url of nb if available.
+        ss_url: url of ss if available.
+    """
+    image = data.get('image')
+    mach = data.get('mach', None)
+    kws = {k:v for k,v in data.items() if k not in ['image', 'mach']}
+    cid, cname, nb_url, ss_url = _create_new_container(image, mach, **kws)
+    return cid, cname, nb_url, ss_url
+
+
+def _create_new_container(image, mach, **kws):
+    global NB_PORT, SS_PORT
+    if mach is not None:
+        # --mach parameter
+        image_name = cname_map[image]
+        command = ['--mach', mach_map[mach]]
+        nb_url, ss_url = None, None
+
+        if 'ss' in image_name:
+            NB_PORT += 1
+            SS_PORT += 1
+            ports = {'8888': ('127.0.0.1', NB_PORT),
+                     '4810': ('127.0.0.1', SS_PORT)}
+        else:
+            NB_PORT += 1
+            ports = {'8888': NB_PORT}
+        
+        try:
+            c = client.containers.create(image_name,
+                    command=command,
+                    ports=ports,
+                    detach=True, tty=True, **kws)
+        except:
+            return None, 'Unknown', nb_url, ss_url
+        
+        nb_url = "http://127.0.0.1:{}".format(NB_PORT)
+        ss_url = "http://127.0.0.1:{}".format(SS_PORT)
+        return c.id, c.name, nb_url, ss_url
