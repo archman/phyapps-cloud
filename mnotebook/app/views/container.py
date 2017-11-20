8b4ed9acf8c4748f51a258ed9752ad9fb89aaa83
Tong Zhang <zhangt@frib.msu.edu>
2017-11-20 09:56:09 -0500

    Update API regarding to the changing of model.

--- mnotebook/app/views/container.py
+++ mnotebook/app/views/container.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from flask import abort
 from flask import request
 from flask import Response
@@ -12,59 +11,78 @@ from flask_restful import fields
 from flask_restful import marshal
 
 from ..utils import validate_container
+from ..utils import create_new_container
 from ..utils import request_json
-
-
-container_fields = {
-    'name': fields.String,
-    'id': fields.String(attribute='id'),
-    'image': fields.String,
-    'status': fields.String,
-}
+from ..fields import container_fields
+from ..models import Container
+from ..models import User
+from ..models import db
 
 
 class ContainerAdminAPI(Resource):
     def __init__(self):
         super(ContainerAdminAPI, self).__init__()
     
-    def get(self, cid):
+    def get(self, name):
         """
         Parameters
         ----------
-        cid : str
-            Container id or name.
+        nam : str
+            Container name.
         """
-        _, container = validate_container(cid)
+        cid, _ = validate_container(name)
+        container = Container.query.filter_by(cid=cid).first()
         if not container:
             abort(404)
+
+        #debug
+        print marshal(container, container_fields)
 
         if request_json():
             return {'container': marshal(container, container_fields)}
         return Response(
                 render_template('show_container.html',
-                    item=marshal(container, container_fields)),
+                    container=marshal(container, container_fields)),
                 mimetype='text/html')
 
-    def post(self, cid):
-        container, _ = validate_container(cid)
+    def post(self, name):
+        # new
+        data = request.get_json()
+        try:
+            new_cid, new_cname, new_url1, new_url2 = \
+                create_new_container({k:v for k,v in data.items() if k not in ('uname')})
+            new_container = Container(cid=new_cid)
+            u = User.query.filter(User.name==data['uname']).first()
+            print(u)
+            print(new_container)
+            if u is None:
+                abort(404)
+            new_container.user = u
+            db.session.add(new_container)
+            db.session.commit()
+            return {'id': new_cid, 'name': new_cname, 'nb_url': new_url1, 'ss_url': new_url2}, 201
+        except:
+            return {'id': None, 'name': 'Unknown', 'nb_url': None, 'ss_url': None}, 501
+
+    def put(self, name):
+        # upate
+        cid, c = validate_container(name)
+        container = Container.query.filter_by(cid=cid).first()
         if not container:
             abort(404)
         op = request.get_json().get('op')
         if op == "stop":
-            if container.status == 'running':
-                return self._stop(container)
+            if c.status == 'running':
+                return self._stop(c), 200
         elif op == "pause":
-            if container.status == 'running':
-                return self._pause(container)
+            if c.status == 'running':
+                return self._pause(c), 200
         elif op == "start":
-            if container.status == 'exited':
-                return self._start(container)
+            if c.status in ('created', 'exited'):
+                return self._start(c), 200
         elif op == "resume":
-            if container.status == 'paused':
-                return self._resume(container)
-        
-        #print(url_for('cid_admin', cid=cid, _scheme="https", _external=True))
-        #return redirect(url_for('cid_admin', cid=cid))
+            if c.status == 'paused':
+                return self._resume(c), 200
         
     def _stop(self, c):
         print("{} has just been stopped.".format(c.id))
