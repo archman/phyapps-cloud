96694dd237c51052f8c88a6beea50af68aa846ae
Tong Zhang <zhangt@frib.msu.edu>
2017-12-01 16:59:16 -0500

    Switch to MySQL.

--- mnotebook/config.py
+++ mnotebook/config.py
@@ -5,11 +5,14 @@ DEBUG = True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#SQLALCHEMY_DATABASE_URI = \
-#    'mysql+pymysql://{username}:{password}@{host}/unicorn'.format(
-#        username='dev',
-#        password='dev',
-#        host='localhost')
-#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLite
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# MySQL
+SQLALCHEMY_DATABASE_URI = \
+    'mysql+pymysql://{username}:{password}@{host}/phyapps_cloud'.format(
+        username='dev',
+        password='dev',
+        host='localhost')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
